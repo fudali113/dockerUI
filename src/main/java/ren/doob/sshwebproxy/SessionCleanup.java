4959593bc0feb37c3ee220bc539b9b47607c1a0a
fudali113 <fudali113@gmail.com>
2015-12-31 15:51:51 +0800

    删除不必要的文件

--- src/main/java/ren/doob/sshwebproxy/SessionCleanup.java
+++ src/main/java/ren/doob/sshwebproxy/SessionCleanup.java
@@ -1,42 +1,4 @@
-/******************************************************************************
- * $Source: /cvsroot/sshwebproxy/src/java/com/ericdaugherty/sshwebproxy/SessionCleanup.java,v $
- * $Revision: 1.1 $
- * $Author: edaugherty $
- * $Date: 2003/12/07 20:48:18 $
- ******************************************************************************
- * Copyright (c) 2003, Eric Daugherty (http://www.ericdaugherty.com)
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright notice,
- *       this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Eric Daugherty nor the names of its
- *       contributors may be used to endorse or promote products derived
- *       from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- * *****************************************************************************
- * For current versions and more information, please visit:
- * http://www.ericdaugherty.com/dev/sshwebproxy
- *
- * or contact the author at:
- * web@ericdaugherty.com
- *****************************************************************************/
+
 
 package ren.doob.sshwebproxy;
 
@@ -49,12 +11,6 @@ import javax.servlet.http.HttpSession;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-/**
- * Closes any open connections for sessions that
- * have timed out.
- *
- * @author Eric Daugherty
- */
 public class SessionCleanup implements HttpSessionListener {
 
     //***************************************************************
--- src/main/java/ren/doob/sshwebproxy/SshConnection.java
+++ src/main/java/ren/doob/sshwebproxy/SshConnection.java
@@ -1,42 +1,3 @@
-/******************************************************************************
- * $Source: /cvsroot/sshwebproxy/src/java/com/ericdaugherty/sshwebproxy/SshConnection.java,v $
- * $Revision: 1.2 $
- * $Author: edaugherty $
- * $Date: 2003/11/23 00:18:10 $
- ******************************************************************************
- * Copyright (c) 2003, Eric Daugherty (http://www.ericdaugherty.com)
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright notice,
- *       this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Eric Daugherty nor the names of its
- *       contributors may be used to endorse or promote products derived
- *       from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- * *****************************************************************************
- * For current versions and more information, please visit:
- * http://www.ericdaugherty.com/dev/sshwebproxy
- *
- * or contact the author at:
- * web@ericdaugherty.com
- *****************************************************************************/
 
 package ren.doob.sshwebproxy;
 
@@ -63,14 +24,6 @@ import java.net.UnknownHostException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-/**
- * SshConnection represents an Ssh connection
- * between the local host and a remote ssh daemon.
- * A single SshConnection may contain multiple channels,
- * which may be file transfer channels or shell channels.
- *
- * @author Eric Daugherty
- */
 public class SshConnection implements SshConstants {
 
     //***************************************************************
