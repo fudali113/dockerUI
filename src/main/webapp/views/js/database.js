90916b08e282f17d469cab2442dedba211142d1e
fudali113 <fudali113@gmail.com>
2016-03-29 11:44:34 +0800

    数据库页面已基本搞定，准备开发后端逻辑

--- src/main/webapp/views/js/database.js
+++ src/main/webapp/views/js/database.js
@@ -3,4 +3,12 @@ app.controller('datacon', function($scope , $http) {
 
     $scope.databases = []
 
-}
+    $scope.clickimage = function(e){
+        alert(e.target.innerHTML)
+    }
+
+    $scope.createDatabase = function(){
+        $('#myModal').modal('show');
+    }
+
+})
